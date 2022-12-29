2cb126f424a6ff26e7ca460af1e933b8e1fe447a
iyanuloluwa-Miracle <iyanudina@gmail.com>
2022-12-29 09:08:21 +0100

    added few changes

--- index.js
+++ index.js
@@ -3,83 +3,89 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash');
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-const aboutContent = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,comes from a line in section 1.10.32";
-const contactContent = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
-
+const homeStartingContent =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+const aboutContent =
+  "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,comes from a line in section 1.10.32";
+const contactContent =
+  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+
+const postSchema = {
+  title: String,
+  content: String,
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 let posts = [];
 
 //how to display home.ejs on the local host
-app.get("/", function(req, res){
-  res.render("home", {
-    startingContent:homeStartingContent, 
-    posts:posts 
-  })//rendering the about content too
-})
-
+app.get("/", function (req, res) {
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts,
+    });
+  });
+});
 
 //how to route about.ejs on the local host
-app.get("/about", function(req, res){
-  res.render("about", {newaboutContent:aboutContent })//rendering the about content too
-})
+app.get("/about", function (req, res) {
+  res.render("about", { newaboutContent: aboutContent }); //rendering the about content too
+});
 
 //how to route contact.ejs on the local host
-app.get("/contact", function(req, res){
-  res.render("contact", {newContactus:contactContent })//rendering the about content too
-})
-
-
-
+app.get("/contact", function (req, res) {
+  res.render("contact", { newContactus: contactContent }); //rendering the about content too
+});
 
 //how to push form compose about.ejs on the local host
-app.get("/compose", function(req, res){
-  res.render("compose")//rendering the about content too
-})
+app.get("/compose", function (req, res) {
+  res.render("compose"); //rendering the about content too
+});
 //Tapping into the compose form
-app.post("/compose", function(req, res){
+app.post("/compose", function (req, res) {
   //creating an object for the form.
   //console.log(req.body.postTitle)
   //creating an object for the form.
-  const post = {
-    title:req.body.postTitle,
-    content: req.body.postBody
-  }
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
   //redirecting after the form is submitted
-  posts.push(post)  
-  res.redirect("/")
-})
+  post.save();
+  res.redirect("/");
+});
 
 //getting the parameter
 
-app.get("/posts/:postName", function(req, res){
+app.get("/posts/:postName", function (req, res) {
   //using lodash to get the parameter
   const requestedTitle = _.lowerCase(req.params.postName);
   //looping through each of the post using the for each array
-  posts.forEach(function(post){
+  posts.forEach(function (post) {
     let storedTitle = _.lowerCase(post.title);
 
-    if(storedTitle === requestedTitle){
-      res.render("post",{
-        title:post.title,
-        content:post.content
-      })
-      
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
     }
-    
-  })
-})
+  });
+});
 
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
